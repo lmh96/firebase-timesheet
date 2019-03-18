e30a87c94a542d8adba7f7d335eca76748f441dd
lmh96 <loganhill23@gmail.com>
2019-03-18 18:49:48 -0400

    created filestructure

---
+++ assets/js/functionalith.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAIYmB7bsSOzT8yZCp9PtAc_CR9t8hmJC8",
+    authDomain: "timesheet-8bdaa.firebaseapp.com",
+    databaseURL: "https://timesheet-8bdaa.firebaseio.com",
+    projectId: "timesheet-8bdaa",
+    storageBucket: "timesheet-8bdaa.appspot.com",
+    messagingSenderId: "197256717814"
+  };
+  firebase.initializeApp(config);
