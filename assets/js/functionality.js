92e17d1d33455ab0d50be9a04e678f4df24a6a78
lmh96 <loganhill23@gmail.com>
2019-03-18 20:03:20 -0400

    added base sign up page funcitonality

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -1,15 +1,17 @@
 // Initialize Firebase
-// var config = {
-//     apiKey: "AIzaSyAIYmB7bsSOzT8yZCp9PtAc_CR9t8hmJC8",
-//     authDomain: "timesheet-8bdaa.firebaseapp.com",
-//     databaseURL: "https://timesheet-8bdaa.firebaseio.com",
-//     projectId: "timesheet-8bdaa",
-//     storageBucket: "timesheet-8bdaa.appspot.com",
-//     messagingSenderId: "197256717814"
-//   };
-//   firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyAIYmB7bsSOzT8yZCp9PtAc_CR9t8hmJC8",
+  authDomain: "timesheet-8bdaa.firebaseapp.com",
+  databaseURL: "https://timesheet-8bdaa.firebaseio.com",
+  projectId: "timesheet-8bdaa",
+  storageBucket: "timesheet-8bdaa.appspot.com",
+  messagingSenderId: "197256717814"
+};
+firebase.initializeApp(config);
 
-$("#submit-btn").on("click", function() {
+var database = firebase.database();
+
+$("#submit-btn").on("click", function () {
 
   let tempRow = $("<div>");
   let colOne = $("<div>");    //employee name
@@ -43,3 +45,25 @@ $("#submit-btn").on("click", function() {
 
   $("#tableContents").append(tempRow);
 });
+
+$("#submitBtn").on("click", function() {
+  let d = new Date();
+  let email = $("#emailInput").val();
+  email = email.substr(0, email.indexOf("."));
+  let name = $("#nameInput").val();
+  let role = $("#roleInput").val();
+  let monthlyRate = $("#monthlyRateInput").val();
+  let startDate = d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear();
+
+  writeEmployeeData(email, name, role, startDate, monthlyRate);
+});
+
+function writeEmployeeData(email, name, role, startDate, monthlyRate)
+{
+  database.ref('employees/' + email).set({
+    Name: name,
+    Role: role,
+    Start_Date: startDate,
+    Monthly_Rate: monthlyRate
+  });
+};
